c0c99febbc878d3517b7628cdc3565256e5f23d8
Vacuda <adamjsikora@gmail.com>
2020-03-12 10:52:14 -0500

    Removing MailGun Info

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,16 +6,16 @@ class ApplicationMailer < ActionMailer::Base
 
   def coin_low
 
-    puts "into email method"
+    puts "Send email message to"
     
-    def send_simple_message
-      RestClient.post "https://api:2ccf2ed59be9cc57e20fd12f1e0c8cdf-ee13fadb-22d9b76b"\
-      "@api.mailgun.net/v3/sandbox813b7193c5804af285d26ed7f7390962.mailgun.org/messages",
-      :from => "Excited User <mailgun@sandbox813b7193c5804af285d26ed7f7390962>",
-      :to => "adamjsikora@gmail.com",
-      :subject => "Hello",
-      :text => "Testing some Mailgun awesomness!"
-    end
+    # def send_simple_message
+    #   RestClient.post "https://api:###########"\
+    #   "@api.mailgun.net/v3/##########.mailgun.org/messages",
+    #   :from => "Excited User <mailgun@sandbox813b7193c5804af285d26ed7f7390962>",
+    #   :to => "adamjsikora@gmail.com",
+    #   :subject => "Hello",
+    #   :text => "Testing some Mailgun awesomness!"
+    # end
 
 
 
